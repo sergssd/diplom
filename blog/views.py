1afbc11c6cee9fe15306e0dc4e9072ad1604df54
sergssd <ae350754>
2016-12-24 14:31:43 +0200

    2st step

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
 
+
 def index(request):
-    return render(request, '/blog/index.html')
+    return render(request, 'blog/index.html')
--- diplom/urls.py
+++ diplom/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 from blog import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls')),
+    url(r'', include('blog.urls')),
 ]
